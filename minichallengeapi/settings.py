e7252e41c1bd1cdc608f594225a0931112426618
henrique-a <henriquearaujoandrade@gmail.com>
2018-06-14 16:45:38 -0300

    hotfix

--- minichallengeapi/settings.py
+++ minichallengeapi/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'minichallengeapi.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': dj_database_url.config()
 }
 
 
