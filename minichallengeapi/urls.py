277770dc806df39e017dbe860baa7e77af91fe68
henrique-a <henriquearaujoandrade@gmail.com>
2018-06-14 17:18:44 -0300

    admin url

--- minichallengeapi/urls.py
+++ minichallengeapi/urls.py
@@ -25,6 +25,7 @@ router.register(r'recipes', views.RecipeViewSet)
 
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
