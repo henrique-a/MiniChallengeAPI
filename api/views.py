670a955f14d3fa7f24206d5b1f599330e01873b3
henrique-a <henriquearaujoandrade@gmail.com>
2018-06-15 10:53:05 -0300

    ingridient view

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from . import views
-from api.models import Recipe
+from api.models import Recipe, Ingridient
 from rest_framework import viewsets
-from api.serializers import RecipeSerializer
+from api.serializers import RecipeSerializer, IngridientSerializer
 
 # Create your views here.
 
@@ -10,3 +10,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
     
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+class IngridientViewSet(viewsets.ModelViewSet):
+    
+    queryset = Ingridient.objects.all()
+    serializer_class = IngridientSerializer
