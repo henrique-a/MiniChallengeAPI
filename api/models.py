beca98f97b4c631450af4e3005a3d13fd1c7893b
henrique-a <henriquearaujoandrade@gmail.com>
2018-06-15 11:31:31 -0300

    hotfix

--- api/models.py
+++ api/models.py
@@ -15,7 +15,7 @@ class Ingridient(models.Model):
     description = models.CharField(max_length=50, default="")
     quantity = models.FloatField(default=1.0)
     unity = models.CharField(max_length=20, default="")
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.description
 
