7bf0ca8370cff958c06b90af258cd9db68559da2
henrique-a <henriquearaujoandrade@gmail.com>
2018-06-15 10:43:39 -0300

    mudança no RecipeSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from api.models import Recipe
 from api.models import Ingridient
 
@@ -12,3 +12,14 @@ class RecipeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Recipe
         fields = '__all__'
+
+    def create(self, validated_data):
+        
+        ingridient_data = validated_data.pop('ingridient')
+        ingridient = IngridientSerializer.create(IngridientSerializer(), validated_data=ingridient_data)
+        recipe, created = Recipe.objects.update_or_create(name=validated_data.pop('name'),
+                            howToPrepare=validated_data.pop('howToPrepare'),
+                            timeToPrepare=validated_data.pop('timeToPrepare'),
+                            portions=validated_data.pop('portions'),
+                            ingridient=ingridient)
+        return recipe
