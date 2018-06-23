13deba72d548ab08cfed2b303796c36374284358
Henrique Santos de Andrade <henriquewsd@yahoo.com>
2018-06-23 18:37:59 -0300

    Adicionado primary key em recipe

--- api/models.py
+++ api/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=50, default="")
+    name = models.CharField(max_length=50, default="", primary_key=True)
     howToPrepare = models.TextField(default="")
-    timeToPrepare = models.IntegerField(default=1)
-    portions = models.IntegerField(default=1)
+    timeToPrepare = models.IntegerField(default=0)
+    portions = models.IntegerField(default=0)
     image = models.ImageField(null=True, blank=True, upload_to='img')
     favorite = models.BooleanField(default=False)
 
@@ -15,9 +15,9 @@ class Recipe(models.Model):
 
 class Ingridient(models.Model):
     description = models.CharField(max_length=50, default="")
-    quantity = models.FloatField(default=1.0)
+    quantity = models.FloatField(default=0.0)
     unity = models.CharField(max_length=20, default="")
-    price = models.FloatField(default=1.0)
+    price = models.FloatField(default=0.0)
     recipe = models.ForeignKey(Recipe, related_name='ingridients', on_delete=models.CASCADE, null=True)
     
     def __str__(self):
